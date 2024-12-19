90c06783e1db058133a895ac0c6509652049b34a
chelsea0111 <xuxinyi9977@gmail.com>
2024-12-19 15:38:31 +0800

    GetEndPoint()

--- P02-Routing/Program.cs
+++ P02-Routing/Program.cs
@@ -1,8 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Endpoint? endpoint = context.GetEndpoint(); // it will be null if it's before app.UseRouting();
+    Console.WriteLine(endpoint == null);
+    await next();
+});
+
 // enable routing
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? endpoint = context.GetEndpoint(); 
+    Console.WriteLine(endpoint == null);
+    await next();
+});
 
 // creating end points
 app.UseEndpoints((IEndpointRouteBuilder endpoints) =>
@@ -13,9 +27,6 @@ app.UseEndpoints((IEndpointRouteBuilder endpoints) =>
         async (context) => await context.Response.WriteAsync("In Map 2"));
 });
 
-app.Run(async context =>
-{
-    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
-});
+app.Run(async context => { await context.Response.WriteAsync($"Request received at {context.Request.Path}"); });
 
 app.Run();
