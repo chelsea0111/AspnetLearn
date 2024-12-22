1d45ddca101981ee666b3897ccc9f74724b29e45
chelsea0111 <xuxinyi9977@gmail.com>
2024-12-22 23:57:32 +0800

    return ContentResult

--- P03-Controllers/Controllers/HomeController.cs
+++ P03-Controllers/Controllers/HomeController.cs
@@ -2,22 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace P03_Controllers.Controllers;
 
-public class HomeController
+public class HomeController : Controller
 {
     // 3. attribute routing
     [Route("home")]
     [Route("/")]
-    public string Index()
+    public ContentResult Index()
     {
-        return "Hello from home!";
+        // return new ContentResult()
+        // {
+        //     Content = "Hello from home!~~~", ContentType = "text/plain"
+        // };
+        
+        // to use this predefined method, the XxxController must inherit from the Controller parent class
+        return Content("<h1>Hello from home!~~~!!!</h1> <h2>I'm h2</h2>", "text/html");
     }
-    
+
     [Route("about")]
     public string About()
     {
         return "Hello from about!";
     }
-    
+
     [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
     public string Contact()
     {
