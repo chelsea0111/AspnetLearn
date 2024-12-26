73f044ffb6de942e5cb2c14cb4dd423802de6a60
chelsea0111 <153299620+chelsea0111@users.noreply.github.com>
2024-12-26 16:09:46 +0800

    test

--- P04-Model-Validation/Models/Person.cs
+++ P04-Model-Validation/Models/Person.cs
@@ -29,7 +29,6 @@ public class Person
 
     [Range(0, 99.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
     public double? Price { get; set; }
-
     
     public override string ToString()
     {
