96f6cec4f473e6e6d806676baefc0540d83c5e20
chelsea0111 <xuxinyi9977@gmail.com>
2024-12-30 17:24:08 +0800

    strongly typed views

--- P05-Views/Controllers/HomeController.cs
+++ P05-Views/Controllers/HomeController.cs
@@ -25,4 +25,33 @@ public class HomeController : Controller
         // ViewBag.people = people;
         return View(people);
     }
+
+    [Route("person-details/{name}")]
+    public IActionResult Details(string? name)
+    {
+        if (name == null)
+        {
+            return Content("Person name can't be null");
+        }
+
+        List<Person> people = new List<Person>()
+        {
+            new Person() { Name = "Tom", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male },
+            new Person() { Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female },
+            new Person() { Name = "Susan", DateOfBirth = null, PersonGender = Gender.Other },
+        };
+        Person? matchingPerson = people.FirstOrDefault(temp => temp.Name == name);
+        return View(matchingPerson); // Views/Home/Details.cshtml
+    }
+
+    [Route("person-with-product")]
+    public IActionResult PersonWithProduct()
+    {
+        Person person = new Person()
+            { Name = "Sara", PersonGender = Gender.Female, DateOfBirth = DateTime.Parse("1998-05-24") };
+        Product product = new Product() { ProductId = 1, ProductName = "Air Conditioner" };
+        PersonAndProductWrapperModel personAndProductWrapperModel = new PersonAndProductWrapperModel()
+            { PersonData = person, ProductData = product };
+        return View(personAndProductWrapperModel);
+    }
 }
