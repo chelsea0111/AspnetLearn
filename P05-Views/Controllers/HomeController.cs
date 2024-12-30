5a056bd1d52d9bf964de6b801dc942a9ba74534c
chelsea0111 <xuxinyi9977@gmail.com>
2024-12-30 10:30:44 +0800

    ViewData

--- P05-Views/Controllers/HomeController.cs
+++ P05-Views/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using P05_Views.Models;
 
 namespace P05_Views.Controllers;
 
@@ -13,6 +15,14 @@ public class HomeController : Controller
 
         // default view name is: Index.cshtml
         // the view is searched and expected to be in the 'Views/Home/Index.cshtml'
+        ViewData["appTitle"] = "Asp.Net Core App~";
+        List<Person> people = new List<Person>()
+        {
+            new Person() { Name = "Tom", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male },
+            new Person() { Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female },
+            new Person() { Name = "Susan", DateOfBirth = DateTime.Parse("2008-07-12"), PersonGender = Gender.Other },
+        };
+        ViewData["people"] = people;
         return View();
     }
 }
