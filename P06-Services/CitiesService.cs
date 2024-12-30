190da4769fb8b34248e462a4d9bbf8e25659f386
chelsea0111 <xuxinyi9977@gmail.com>
2024-12-30 14:10:51 +0800

    DI init

---
+++ P06-Services/CitiesService.cs
@@ -0,0 +1,20 @@
+﻿namespace P06_Services;
+
+public class CitiesService
+{
+    private List<string> _cities;
+
+    public CitiesService(List<string> cities)
+    {
+        _cities = new List<string>()
+        {
+            "London",
+            "Paris",
+            "New York",
+            "Tokyo",
+            "Rome"
+        };
+    }
+
+    public List<string> GetCities() => _cities;
+}
