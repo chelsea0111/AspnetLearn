060b307e0c89432db5f7b50e76223c1803288dc0
chelsea0111 <xuxinyi9977@gmail.com>
2025-01-14 22:15:19 +0800

    The 2nd way to map conf to an object: using Bind() method

--- P10-Configuration/Controllers/HomeController.cs
+++ P10-Configuration/Controllers/HomeController.cs
@@ -17,12 +17,17 @@ public class HomeController : Controller
         ViewBag.MyKey1 = _configuration["MyKey"];
         ViewBag.MyKey2 = _configuration.GetValue<string>("x", "default");
 
-        // ViewBag.ClientID = _configuration["WeatherApi:ClientID"];
-        // ViewBag.ClientSecret = _configuration.GetValue<string>("WeatherApi:ClientSecret", "default secret");
+        // WeatherApiOptions? weatherApiOptions = _configuration.GetSection("WeatherApi").Get<WeatherApiOptions>();
+        // ViewBag.ClientID = weatherApiOptions.ClientID;
+        // ViewBag.ClientSecret = weatherApiOptions.ClientSecret;
 
-        WeatherApiOptions? weatherApiOptions = _configuration.GetSection("WeatherApi").Get<WeatherApiOptions>();
-        ViewBag.ClientID = weatherApiOptions.ClientID;
-        ViewBag.ClientSecret = weatherApiOptions.ClientSecret;
+        WeatherApiOptions options = new WeatherApiOptions();
+        IConfigurationSection confSection = _configuration.GetSection("WeatherApi");
+
+        // Loads conf values into a new Options object
+        confSection.Bind(options);
+        ViewBag.ClientID = options.ClientID;
+        ViewBag.ClientSecret = options.ClientSecret;
 
         return View();
     }
